9a8c98caa992379aad17c9f5366e36549a68e105
Aarav <aaravarora5600@gmail.com>
2022-07-27 16:15:51 +0530

    still fixin

--- routers/routers.go
+++ routers/routers.go
@@ -6,10 +6,15 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Routes() *fiber.App {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization ",
+	}))
 	app.Post("/short", controllers.Short)
 	app.Static("/", "./static")
 	app.Get("/+", controllers.Shortend)
